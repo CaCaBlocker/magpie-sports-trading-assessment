1d8db349f9442058a1fca88e194b086a3c6b7bea
CaCaBlocker <umamlikeyou@gmail.com>
2023-06-22 14:19:42 +0900

    docs: add comments

--- src/services/getMarketPriceData.ts
+++ src/services/getMarketPriceData.ts
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+/**
+ * The function retrieves market price data from a specified URL using the axios library in TypeScript.
+ * @returns The `getMarketPriceData` function is returning the data obtained from the API endpoint
+ * `https://mocki.io/v1/70f45519-0232-463b-bd4f-88e9d7213d26` using the Axios library. The data
+ * returned is in the form of a Promise, which resolves to the actual data once the API call is
+ * complete.
+ */
 const getMarketPriceData = async () => {
     const result = await axios.get('https://mocki.io/v1/70f45519-0232-463b-bd4f-88e9d7213d26');
     return result.data;
--- src/utils/price.ts
+++ src/utils/price.ts
@@ -1,2 +1,13 @@
+/**
+ * The function takes a string representing a price and returns a number by removing the currency
+ * symbol and commas.
+ * @param {string} priceString - A string representing a price value, with a currency symbol at the
+ * beginning and commas separating thousands. For example, ",234.56".
+ */
 export const convertToNumber = (priceString: string):number => Number(priceString.slice(1).split(',').join(''));
+/**
+ * The function converts a number to a string representing a price with two decimal places and a dollar
+ * sign.
+ * @param {number} price - a number representing a price value.
+ */
 export const convertToPrice = (price: number): string => "$" + price.toFixed(2);
